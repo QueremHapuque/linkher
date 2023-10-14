bcb473a86b16f5152743bcbc56fe860f47da19d7
Diogo <di_smgomes@hotmail.com>
2023-10-14 12:51:01 -0300

    formatter configurations eslint, prettier, editorconfig

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,18 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
 
 const server = express();
 const { SERVER_PORT } = process.env;
-  
-server.use(express.json());
-server.use(cors({
-  origin: "*",
-  optionsSuccessStatus: 200
-}));
 
-server.listen(SERVER_PORT || "3001", async () => {
+server.use(express.json());
+server.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+  }),
+);
+
+server.listen(SERVER_PORT || '3001', async () => {
   console.log(`server running in ${SERVER_PORT} port...`);
 });
