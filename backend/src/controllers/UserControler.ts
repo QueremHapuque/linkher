8055f00a6fad28f7ed24fdc9ad7eb1d4f4139541
Diogo-d-Souza <di_smgomes@hotmail.com>
2023-11-08 20:20:06 -0300

    feat: user edit created

--- backend/src/controllers/UserControler.ts
+++ backend/src/controllers/UserControler.ts
@@ -16,6 +16,25 @@ class UserController {
       res.status(400).json({ erros: e.errors.map((err: any) => err.message) });
     }
   }
+
+  async update(req: Request, res: Response) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ error: 'id is required' });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ error: 'user not found' });
+      }
+
+      const newData = await user.update(req.body);
+      return res.json(newData);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
 }
 
 export default new UserController();
--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -4,5 +4,6 @@ import UserControler from '../controllers/UserControler';
 const router = express.Router();
 
 router.post('/users/', UserControler.create);
+router.put('/user/update/:id', UserControler.update);
 
 export default router;
