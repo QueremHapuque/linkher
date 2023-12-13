2e9e43ff7d301ea25d32e1efc16c255b32e0f70d
lucaslima433 <lucaslima433@hotmail.com>
2023-12-12 21:35:14 -0300

    add: Tela sobre nos

---
+++ frontend/src/app/sobrenos/sobrenos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobrenos',
+  templateUrl: './sobrenos.component.html',
+  styleUrls: ['./sobrenos.component.css']
+})
+export class SobrenosComponent {
+
+}
