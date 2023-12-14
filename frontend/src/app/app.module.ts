809df78795866fd32e1abfbd52d4636906363e97
Diego Diniz <diegodcg064@gmail.com>
2023-12-14 16:00:48 -0300

    feat: initial opportunities screen

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { ChangeEmailModalComponent } from './change-email-modal/change-email-mod
 import { ChangePasswordModalComponent } from './change-password-modal/change-password-modal.component';
 import { DeleteAccountModalComponent } from './delete-account-modal/delete-account-modal.component';
 import { SobrenosComponent } from './sobrenos/sobrenos.component';
+import { OpportunitiesComponent } from './opportunities/opportunities.component';
 
 
 @NgModule({
@@ -49,7 +50,8 @@ import { SobrenosComponent } from './sobrenos/sobrenos.component';
     RegisterComponent,
     ChangePasswordModalComponent,
     DeleteAccountModalComponent,
-    SobrenosComponent
+    SobrenosComponent,
+    OpportunitiesComponent
   ],
   imports: [
     RouterModule,
---
+++ frontend/src/app/opportunities/opportunities.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opportunities',
+  templateUrl: './opportunities.component.html',
+  styleUrls: ['./opportunities.component.css']
+})
+export class OpportunitiesComponent {
+
+}
